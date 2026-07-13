62cadea0832c7ef1246fd2807640ee9aabdde23f
Josh Carlson <22630098+paperman5@users.noreply.github.com>
2026-07-12 17:48:37 -0700

    Add main menu and very basic save/load progress

--- game/game_manager.gd
+++ game/game_manager.gd
@@ -5,8 +5,30 @@ var level : Level
 var game_ui : GameUI
 var menus : WinLoseMenus
 
+const save_file_path := "user://savegame.txt"
+
+const level_scenes = {
+	"main_menu" : "uid://6kyebxi5bbkj",
+	"test" : "uid://bfcba71eywal4"
+}
+
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func time_up():
 	pass
+
+func change_scene(new_scene : String):
+	if new_scene == null or new_scene == "":
+		push_warning("Scene not given")
+	elif not new_scene in level_scenes.keys():
+		push_warning("Scene %s doesn't have a target UID" % new_scene)
+	else:
+		get_tree().change_scene_to_file(level_scenes[new_scene])
+		if new_scene != "main_menu":
+			var save_file = FileAccess.open(save_file_path, FileAccess.WRITE)
+			save_file.store_line(level_scenes[new_scene])
+			save_file.close()
+
+func change_scene_uid(uid : String):
+	get_tree().change_scene_to_file(uid)
---
+++ game/main_menu.gd
@@ -0,0 +1,27 @@
+class_name MainMenu
+extends Control
+
+@onready var continue_button := %Continue as Button
+@onready var newgame_button := %NewGame as Button
+var continue_load := ""
+
+func _ready() -> void:
+	if FileAccess.file_exists(GameManager.save_file_path):
+		continue_button.show()
+		var file = FileAccess.open(GameManager.save_file_path, FileAccess.READ)
+		continue_load = file.get_line()
+		file.close()
+	else:
+		continue_button.hide()
+
+func _on_continue_pressed() -> void:
+	GameManager.change_scene_uid(continue_load)
+
+
+func _on_new_game_pressed() -> void:
+	GameManager.change_scene("test")
+
+
+func _on_credit_meta_clicked(meta: Variant) -> void:
+	# open the link in a web browser.
+	OS.shell_open(str(meta))
